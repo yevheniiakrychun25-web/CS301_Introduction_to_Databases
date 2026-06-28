f3740914022213879555f9b36520b7a703f624d8
evgeniakricun <yevheniia.krychun.25@kse.org.ua>
2026-06-28 14:21:23 +0300

    Added 3 users, view, procedure, triger and function

--- Assignment_4_SQL/assignment_4_sql.sql
+++ Assignment_4_SQL/assignment_4_sql.sql
@@ -54,3 +54,71 @@ create table playlist_songs(
 	added_at timestamp default current_timestamp,
 	primary key (playlist_id, song_id)
 );
+
+create user app_admin
+with password 'admin123';
+grant all privileges on all tables
+in schema public to app_admin;
+
+create user app_analyst
+with password 'analyst123';
+grant select on all tables
+in schema public to app_analyst;
+
+create user app_client
+with password 'client123';
+grant select, insert, update, delete on all tables
+in schema public to app_client;
+
+create view active_user_view as
+select u.user_id,
+	u.username,
+	u.email,
+	s.title,
+	s.price
+from users u 
+left join subscription s 
+on u.subscription_id = s.subscription_id;
+
+create or replace procedure change_user_subscription(p_user_id int, p_new_sub_id int)
+language plpgsql
+as $$
+begin
+	update users
+	set subscription_id = p_new_sub_id
+	where user_id = p_user_id;
+end;
+$$;
+
+create or replace function create_default_settings()
+returns trigger as $$
+begin
+	insert into user_settings(user_id) values
+	(new.user_id);
+	return new;
+end;
+$$
+language plpgsql;
+
+create trigger trigger_new_user_settings
+after insert on users
+for each row
+execute function create_default_settings();
+
+insert into subscription(title, price, device_limit) values
+('Free', 0.00, 1),
+('Premium', 9.99, 5);
+
+insert into users(username, email, subscription_id) values 
+('test_girl', 'test@example.com', 1);
+
+select *
+from user_settings;
+
+select *
+from active_user_view;
+
+call change_user_subscription(1, 2);
+
+select *
+from users;
